469699471fdb85deed65fb388a832d51daa90e42
Anant Sharma <62539702+anantveative@users.noreply.github.com>
2021-04-04 22:08:21 +0530

    Update lyc_veative.sql
    Updated DB

--- lyc_veative.sql
+++ lyc_veative.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2021 at 04:59 PM
+-- Generation Time: Apr 04, 2021 at 22:06 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
